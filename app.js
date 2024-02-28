d6fa8fedf0cb9f93e0e2ce2ce22c4d2cf1cffdf6
Oleksii Bieliaiev <bieliaiev.os@gmail.com>
2024-02-28 17:30:36 +0200

    init versions with scarlett default agent

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const port = 3000;
 
 const app = express();
 app.use('/', express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+app.get('/agent', function(req, res) {
+    res.sendFile(__dirname + '/index-agent.html')
+});
+
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Server started on port localhost:${port}`));
