f642e0d1b4877d23635f15bdfc89eb2186ae1b22
Ofek Simhi <ofek.simhi@deidentification.co>
2024-04-24 20:37:09 +0300

    streaming idle mechanism

--- streaming-client-api.js
+++ streaming-client-api.js
@@ -10,13 +10,16 @@ const RTCPeerConnection = (
 ).bind(window);
 
 let peerConnection;
+let pcDataChannel;
 let streamId;
 let sessionId;
 let sessionClientAnswer;
 
 let statsIntervalId;
-let videoIsPlaying;
 let lastBytesReceived;
+let videoIsPlaying = false;
+const stream_warmup = true; // Set this variable to true to request stream warmup upon connection, aiming to mitigate potential jittering issues
+let isStreamReady = !stream_warmup;
 
 const videoElement = document.getElementById('video-element');
 videoElement.setAttribute('playsinline', '');
@@ -25,6 +28,7 @@ const iceStatusLabel = document.getElementById('ice-status-label');
 const iceGatheringStatusLabel = document.getElementById('ice-gathering-status-label');
 const signalingStatusLabel = document.getElementById('signaling-status-label');
 const streamingStatusLabel = document.getElementById('streaming-status-label');
+const streamEventLabel = document.getElementById('stream-event-label');
 
 const presenterInputByService = {
   talks: {
@@ -32,9 +36,9 @@ const presenterInputByService = {
   },
   clips: {
     presenter_id: 'rian-lZC6MmWfC1',
-    driver_id: 'mXra4jY38i'
-  }
-}
+    driver_id: 'mXra4jY38i',
+  },
+};
 
 const connectButton = document.getElementById('connect-button');
 connectButton.onclick = async () => {
@@ -45,13 +49,18 @@ connectButton.onclick = async () => {
   stopAllStreams();
   closePC();
 
+  /**
+   * Set 'stream_warmup' to 'true' in the payload to initiate idle streaming at the beginning of the connection, addressing jittering issues.
+   * The idle streaming process is transparent to the user and is concealed by triggering a 'stream/ready' event on the data channel,
+   * indicating that idle streaming has concluded and the stream channel is ready for use.
+   */
   const sessionResponse = await fetchWithRetries(`${DID_API.url}/${DID_API.service}/streams`, {
     method: 'POST',
     headers: {
       Authorization: `Basic ${DID_API.key}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(presenterInputByService[DID_API.service]),
+    body: JSON.stringify({ ...presenterInputByService[DID_API.service], stream_warmup }),
   });
 
   const { id: newStreamId, offer, ice_servers: iceServers, session_id: newSessionId } = await sessionResponse.json();
@@ -97,8 +106,8 @@ startButton.onclick = async () => {
         },
         ...(DID_API.service === 'clips' && {
           background: {
-            color: '#FFFFFF'
-          }
+            color: '#FFFFFF',
+          },
         }),
         config: {
           stitch: true,
@@ -146,6 +155,18 @@ function onIceCandidate(event) {
         session_id: sessionId,
       }),
     });
+  } else {
+    // For the initial 2 sec idle stream at the beginning of the connection, we utilize a null ice candidate.
+    fetch(`${DID_API.url}/${DID_API.service}/streams/${streamId}/ice`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${DID_API.key}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        session_id: sessionId,
+      }),
+    });
   }
 }
 function onIceConnectionStateChange() {
@@ -160,6 +181,21 @@ function onConnectionStateChange() {
   // not supported in firefox
   peerStatusLabel.innerText = peerConnection.connectionState;
   peerStatusLabel.className = 'peerConnectionState-' + peerConnection.connectionState;
+  if (peerConnection.connectionState === 'connected') {
+    playIdleVideo();
+    /**
+     * A fallback mechanism: if the 'stream/ready' event isn't received within 5 seconds after asking for stream warmup,
+     * it updates the UI to indicate that the system is ready to start streaming data.
+     */
+    setTimeout(() => {
+      if (!isStreamReady) {
+        console.log('forcing stream/ready');
+        isStreamReady = true;
+        streamEventLabel.innerText = 'ready';
+        streamEventLabel.className = 'streamEvent-ready';
+      }
+    }, 5000);
+  }
 }
 function onSignalingStateChange() {
   signalingStatusLabel.innerText = peerConnection.signalingState;
@@ -167,14 +203,11 @@ function onSignalingStateChange() {
 }
 
 function onVideoStatusChange(videoIsPlaying, stream) {
-  let status;
-  if (videoIsPlaying) {
-    status = 'streaming';
+  const status = videoIsPlaying ? 'streaming' : 'empty';
+
+  if (isStreamReady) {
     const remoteStream = stream;
-    setVideoElement(remoteStream);
-  } else {
-    status = 'empty';
-    playIdleVideo();
+    videoIsPlaying ? setVideoElement(remoteStream) : playIdleVideo();
   }
   streamingStatusLabel.innerText = status;
   streamingStatusLabel.className = 'streamingState-' + status;
@@ -209,15 +242,64 @@ function onTrack(event) {
   }, 500);
 }
 
+function onStreamEvent(message) {
+  /**
+   * This function handles stream events received on the data channel.
+   * The 'stream/ready' event received on the data channel signals the end of the 2sec idle streaming.
+   * Upon receiving the 'ready' event, we can display the streamed video if one is available on the stream channel.
+   * Until the 'ready' event is received, we hide any streamed video.
+   * Additionally, this function processes events for stream start, completion, and errors. Other data events are disregarded.
+   */
+
+  if (pcDataChannel.readyState === 'open') {
+    let status;
+    const [event, _] = message.data.split(':');
+
+    switch (event) {
+      case 'stream/started':
+        status = 'started';
+        break;
+      case 'stream/done':
+        status = 'stop';
+        break;
+      case 'stream/ready':
+        status = 'ready';
+        break;
+      case 'stream/error':
+        status = 'error';
+        break;
+      default:
+        status = 'dont-care';
+        break;
+    }
+
+    // Set stream ready after a short delay, adjusting for potential timing differences between data and stream channels
+    if (status === 'ready') {
+      setTimeout(() => {
+        console.log('stream/ready');
+        isStreamReady = true;
+        streamEventLabel.innerText = 'ready';
+        streamEventLabel.className = 'streamEvent-ready';
+      }, 1000);
+    } else {
+      console.log(event);
+      streamEventLabel.innerText = status === 'dont-care' ? event : status;
+      streamEventLabel.className = 'streamEvent-' + status;
+    }
+  }
+}
+
 async function createPeerConnection(offer, iceServers) {
   if (!peerConnection) {
     peerConnection = new RTCPeerConnection({ iceServers });
+    pcDataChannel = peerConnection.createDataChannel('JanusDataChannel');
     peerConnection.addEventListener('icegatheringstatechange', onIceGatheringStateChange, true);
     peerConnection.addEventListener('icecandidate', onIceCandidate, true);
     peerConnection.addEventListener('iceconnectionstatechange', onIceConnectionStateChange, true);
     peerConnection.addEventListener('connectionstatechange', onConnectionStateChange, true);
     peerConnection.addEventListener('signalingstatechange', onSignalingStateChange, true);
     peerConnection.addEventListener('track', onTrack, true);
+    pcDataChannel.addEventListener('message', onStreamEvent, true);
   }
 
   await peerConnection.setRemoteDescription(offer);
@@ -270,11 +352,15 @@ function closePC(pc = peerConnection) {
   pc.removeEventListener('connectionstatechange', onConnectionStateChange, true);
   pc.removeEventListener('signalingstatechange', onSignalingStateChange, true);
   pc.removeEventListener('track', onTrack, true);
+  pc.removeEventListener('onmessage', onStreamEvent, true);
+
   clearInterval(statsIntervalId);
+  isStreamReady = !stream_warmup;
   iceGatheringStatusLabel.innerText = '';
   signalingStatusLabel.innerText = '';
   iceStatusLabel.innerText = '';
   peerStatusLabel.innerText = '';
+  streamEventLabel.innerText = '';
   console.log('stopped peer connection');
   if (pc === peerConnection) {
     peerConnection = null;
