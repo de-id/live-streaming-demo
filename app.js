a1a347c11b75ddffadc2e3275f992930eb5a7a61
almog100 <almog.gal@deidentification.co>
2025-02-03 15:36:29 +0200

    removed index-ws file

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use('/', express.static(__dirname));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index-ws.html')
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
-app.get('/agents', function(req, res) {
-    res.sendFile(__dirname + '/index-agents.html')
+app.get('/agents', function (req, res) {
+  res.sendFile(__dirname + '/index-agents.html');
 });
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`Server started on port localhost:${port}\nhttp://localhost:${port}\nhttp://localhost:${port}/agents`));
+server.listen(port, () =>
+  console.log(`Server started on port localhost:${port}\nhttp://localhost:${port}\nhttp://localhost:${port}/agents`)
+);
